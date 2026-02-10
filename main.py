a7ed47031dbe6bb7aead597cc3f41095b5303879
nicolascuellosuarez <jaimecuellosuarez@gmail.com>
2026-02-10 15:09:24 -0500

    Using ClassMethod

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from proof2!")
+class Pizza:
+    def __init__(self, ingredientes: list[str]):
+        self.ingredientes: list[str] = ingredientes
 
+    """
+    Factor g
+    """
+
+    @classmethod
+    def crear_hawaiana(cls) -> 'Pizza':
+        return cls(["Queso", "Piña, Jamón"])
+    
+def main():
+    pizza_1 = Pizza(["Chicharrón", "Queso", "Pepperoni"])
+    pizza_hawaiana = Pizza.crear_hawaiana()
+
+    print(pizza_1.ingredientes)
+    print(pizza_hawaiana.ingredientes)
 
 if __name__ == "__main__":
     main()
